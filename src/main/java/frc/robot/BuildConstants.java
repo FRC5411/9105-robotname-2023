c31ac17fccefbe99c1a8b90e3e0ae4a3296fcc2f
Steeltrap77 <aaron.milholland@gmail.com>
2023-03-04 13:24:55 -0600

    changed intake speed

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "9105-robotname-2023";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 97;
-  public static final String GIT_SHA = "7c389769a2a8476b8734a077002f7442541504cf";
-  public static final String GIT_DATE = "2023-03-04 13:38:28 EST";
+  public static final int GIT_REVISION = 98;
+  public static final String GIT_SHA = "e18c44a26ee24db43814e0811462d9d213d81782";
+  public static final String GIT_DATE = "2023-03-04 14:23:24 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2023-03-04 14:08:43 EST";
-  public static final long BUILD_UNIX_TIME = 1677956923778L;
+  public static final String BUILD_DATE = "2023-03-04 14:24:36 EST";
+  public static final long BUILD_UNIX_TIME = 1677957876520L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
